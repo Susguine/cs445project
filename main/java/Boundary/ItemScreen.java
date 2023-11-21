da3df5d629f7dc3df10f122331e4d9087d5fbe2e
Zarzic <93292648+Zarzic@users.noreply.github.com>
2023-11-21 12:38:07 -0600

    Changed the Boundary Classes.
    Now I just need the Control Classes.

--- main/java/Boundary/ItemScreen.java
+++ main/java/Boundary/ItemScreen.java
@@ -5,13 +5,194 @@
 package Boundary;
 
 import Control.ItemController;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  *This class displays items, tasks, and notes
  * @author rschi
  */
-public class ItemScreen {
+public class ItemScreen extends ApplicationGUI{
     private ItemController itemController;
+
+    public static void GetItemScreen(Stage stage, Scene main_menu) {
+        Group grid = new Group();
+        //adding the rungs
+        int y = 15;
+        for(int i = 0; i < 150;i++) {
+            Circle c1 = new Circle(4,4,30);
+            c1.setFill(Color.TRANSPARENT);
+            c1.setStroke(Color.BLACK);
+            c1.setStrokeWidth(8);
+            c1.setCenterX(15);
+            c1.setCenterY(y);
+            grid.getChildren().add(c1);
+            y = y + 15;
+        }
+
+        Button b_b_m = new Button("Back To Main Page");
+        Button ai = new Button("Add Item");
+        Button ei = new Button("Edit Item");
+        Button di = new Button("Delete Item");
+        //evenly space out the buttons
+        HBox btnHB = new HBox(10);
+        btnHB.getChildren().addAll(ai,ei,di,b_b_m);
+        btnHB.setLayoutX(150);
+        btnHB.setLayoutY(10);
+        //adding to the plane of the scene
+        grid.getChildren().add(btnHB);
+        b_b_m.setOnAction(e-> { 
+            stage.setScene(main_menu);
+            });
+        //adding a item button below
+        ai.setOnAction(e-> {
+            //get the number already ready on previous amount entered
+            grid.getChildren().remove(btnHB);
+            TextArea input = new TextArea();
+            TextArea item_note = new TextArea();
+            Text note_label = new Text("Note for Item Below:");
+            Text item = new Text("Enter Item Below:");
+            Button confirm = new Button("Confirm");
+            Button back = new Button("Back");
+            input.setLayoutX(150);
+            input.setLayoutY(50);
+            input.setPrefWidth(400);
+            input.setPrefHeight(400);
+
+            item_note.setLayoutX(150);
+            item_note.setLayoutY(470);
+            item_note.setPrefWidth(400);
+            item_note.setPrefHeight(200);
+            
+            note_label.setLayoutX(150);
+            note_label.setLayoutY(460);
+
+            item.setLayoutX(150);
+            item.setLayoutY(30);
+            HBox centering = new HBox(10);
+            centering.setLayoutX(150);
+            centering.setLayoutY(680);
+            centering.getChildren().addAll(confirm,back);
+            grid.getChildren().addAll(input,centering,item_note,note_label,item);
+            confirm.setOnAction(b-> {
+                    //here we do the controller and apply to the main screen along with a number that says what item number it is!!
+                    //make sure to double check if there is a note added or not
+                    grid.getChildren().removeAll(input,centering,item_note,note_label,item);
+                    grid.getChildren().add(btnHB);
+                });
+            back.setOnAction(c-> {
+                    //there is no processing just getting back to the other page
+                    grid.getChildren().removeAll(input,centering,item_note,note_label,item);
+                    grid.getChildren().add(btnHB);
+                });
+            });
+        //edit item button below
+        ei.setOnAction(e-> {
+            //get the number already ready on previous amount entered
+            grid.getChildren().remove(btnHB);
+            Text number = new Text("Enter Number of Item Below:");
+            Text item = new Text("Change Task Below:");
+            TextArea ion = new TextArea();
+            TextArea ie = new TextArea();
+            TextArea ine = new TextArea();
+            Text note_label = new Text("Change Note for Task Below:");
+            Button confirm = new Button("Confirm Changes");
+            Button cn = new Button("Confirm Number");
+            Button back = new Button("Back");
+
+            cn.setLayoutX(150);
+            cn.setLayoutY(100);
+
+            ion.setLayoutX(150);
+            ion.setLayoutY(60);
+            ion.setPrefWidth(400);
+            ion.setPrefHeight(2);
+
+            ie.setLayoutX(150);
+            ie.setLayoutY(150);
+            ie.setPrefWidth(400);
+            ie.setPrefHeight(300);
+
+            ine.setLayoutX(150);
+            ine.setLayoutY(500);
+            ine.setPrefWidth(400);
+            ine.setPrefHeight(200);
+            
+            note_label.setLayoutX(150);
+            note_label.setLayoutY(480);
+
+            number.setLayoutX(150);
+            number.setLayoutY(50);
+
+            item.setLayoutX(150);
+            item.setLayoutY(140);
+            HBox centering = new HBox(10);
+            centering.setLayoutX(150);
+            centering.setLayoutY(710);
+            centering.getChildren().addAll(confirm,back);
+            grid.getChildren().addAll(ion,ie,centering,ine,note_label,item,number,cn);
+            cn.setOnAction(m-> {
+                    //make sure to get items and fill the Text area from the entities class here
+                });
+            confirm.setOnAction(b-> {
+                    //here we do the controller and apply to the main screen along with a number that says what item number it is and sussefully edit it.
+                    //make sure to double check if there is a note added or not
+                    grid.getChildren().removeAll(ion,ie,centering,ine,note_label,item,number,cn);
+                    grid.getChildren().add(btnHB);
+                });
+            back.setOnAction(c-> {
+                    //there is no processing just getting back to the other page
+                    grid.getChildren().removeAll(ion,ie,centering,ine,note_label,item,number,cn);
+                    grid.getChildren().add(btnHB);
+                });
+            });
+        di.setOnAction(e-> {
+            grid.getChildren().remove(btnHB);
+            Text number = new Text("Enter Number of Item to Delete Below:");
+            TextArea ion = new TextArea();
+            Button confirm = new Button("Confirm Delete");
+            Button back = new Button("Back");
+
+            ion.setLayoutX(150);
+            ion.setLayoutY(60);
+            ion.setPrefWidth(400);
+            ion.setPrefHeight(2);            
+
+            number.setLayoutX(150);
+            number.setLayoutY(50);
+
+            HBox centering = new HBox(10);
+            centering.setLayoutX(150);
+            centering.setLayoutY(100);
+            centering.getChildren().addAll(confirm,back);
+            grid.getChildren().addAll(ion,centering,number);
+            confirm.setOnAction(b-> {
+                    //here we do the controller and apply to the main screen along with a number that says what item number it is!!
+                    //make sure to double check if there is a note added or not
+                    grid.getChildren().removeAll(ion,centering,number);
+                    grid.getChildren().add(btnHB);
+                });
+            back.setOnAction(c-> {
+                    //there is no processing just getting back to the other page
+                    grid.getChildren().removeAll(ion,centering,number);
+                    grid.getChildren().add(btnHB);
+                });
+            });
+        Scene item_page = new Scene(grid,600,800);
+        item_page.setFill(ApplicationGUI.c1);
+        stage.setScene(item_page);
+        
+    }
 
     public ItemController getItemController() {
         return itemController;
--- main/java/Boundary/WeatherScreen.java
+++ main/java/Boundary/WeatherScreen.java
@@ -5,11 +5,46 @@
 package Boundary;
 
 import Entity.Weather;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 /**
  *This class displays weather-related information, such as current conditions, forecasts, and other weather data
  * @author rschi
  */
-public class WeatherScreen {
+public class WeatherScreen extends ApplicationGUI{
     private Weather currentWeather;
+
+    public static void GetWeatherScreen(Stage stage, Scene main_menu) {
+        Group grid = new Group();
+        //adding rungs to the notebook
+        int y = 15;
+        for(int i = 0; i < 150;i++) {
+            Circle c1 = new Circle(4,4,30);
+            c1.setFill(Color.TRANSPARENT);
+            c1.setStroke(Color.BLACK);
+            c1.setStrokeWidth(8);
+            c1.setCenterX(15);
+            c1.setCenterY(y);
+            grid.getChildren().add(c1);
+            y = y + 15;
+        }
+        Button b_b_m = new Button("Back To Main Page");
+        b_b_m.setLayoutX(150);
+        b_b_m.setLayoutY(10);
+        grid.getChildren().add(b_b_m);
+        //person clicks the back button after looking at the weather
+        b_b_m.setOnAction(e-> { 
+            stage.setScene(main_menu);
+            });
+        //AREA TO DISPLAY THE WEATHER INFORMATION THAT i GET FROM THE CONTROLLER. I NEED THE CONTROLLER READY.
+            Scene weather_page = new Scene(grid,600,800);
+            weather_page.setFill(ApplicationGUI.c1);
+            stage.setScene(weather_page);
+    }
 }
