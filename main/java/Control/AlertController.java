0728be24aa7c4831e5e6658a7f705cd820861c52
Susguine <tyration@gmail.com>
2023-11-25 20:05:51 -0600

    add alert controller code

--- main/java/Control/AlertController.java
+++ main/java/Control/AlertController.java
@@ -4,11 +4,22 @@
  */
 package Control;
 
+import Entity.Alert;
+import Entity.Date;
+
+
 /**
  * This class controls and manages the flow of alert-related tasks and data within the application, 
  * facilitating the creation, editing, and management of alerts and ensuring they are triggered at the specified times
  * @author rschi
  */
 public class AlertController {
-    
+    public Alert sendAlert(Alert alert) {
+
+        return alert; // delete this and insert send alert logic
+    }
+
+    public Alert createAlert(String title, Date date) {
+        return new Alert(title, date);
+    }
 }
