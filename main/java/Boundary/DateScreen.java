f74221e4c2610bd0f1abd80e93566b89b2d0acb8
Susguine <tyration@gmail.com>
2023-11-27 11:48:41 -0600

    remove DateController

--- main/java/Boundary/DateScreen.java
+++ main/java/Boundary/DateScreen.java
@@ -4,13 +4,9 @@
  */
 package Boundary;
 
-import Control.DateController;
-
 /**
  * This class displays dates, tasks, and other date-related information, 
  * 
  * @author rschi
  */
-public class DateScreen {
-    private DateController dateController;
-}
+
